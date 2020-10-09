a3f8ac327d4d3865f1e9ac3eabfcbf47256e47bc
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2020-10-09 12:45:44 +0530

    Create a.java

---
+++ a.java
@@ -0,0 +1,22 @@
+        int n = s.length();
+        int maxPow = 0;
+        
+        for( int i=0; i < n; i++)
+        {
+            String subStr = s.substring(0, n);
+            if(subStr.charAt(n-1) != '1')
+            {   
+                int count = 0;
+                int num = Integer.parseInt(subStr,2);
+                while(num % 2 == 0)
+                {
+                    count++;
+                }
+                if(maxPow < count)
+                {
+                    maxPow = count;
+                }
+            }
+            s = subStr.valueOf(n-1) + subStr.substring(0, n-1);
+        }
+        return maxPow;
